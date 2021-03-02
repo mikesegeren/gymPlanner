559fd2e8d7f043e92a2ca77ba3013cb8f7c82c2d
MikeS <m.segeren@loket.nl>
2021-03-02 15:23:25 +0100

    init

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const cron = require("node-cron");
 let shell = require("shelljs");
 
 // At 12:00 on Sunday, Tuesday, and Thursday.
+// 00 12 * * 7,2,4
 cron.schedule("00 12 * * 7,2,4", () => {
-  console.log("running");
   if (shell.exec("node book.js").code !== 0) {
       console.log('Something went wrong');
   }
