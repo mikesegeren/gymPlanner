3184e93a2186dab3a1f63bcc8103ae3672509a99
MikeS <m.segeren@loket.nl>
2021-03-04 11:52:24 +0100

    fix

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let shell = require("shelljs");
 console.log('running');
 // At 12:00 on Sunday, Tuesday, and Thursday.
 // 00 12 * * 7,2,4
-cron.schedule("00 12 * * 7,2,4", () => {
+cron.schedule("05 12 * * 7,2,4", () => {
   if (shell.exec("node book.js").code !== 0) {
       console.log('Something went wrong');
   }
